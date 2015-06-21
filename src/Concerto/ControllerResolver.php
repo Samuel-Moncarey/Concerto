79f4f50a692296614697b5c8d12b5c311a8f2dca
Samuel Moncarey <samuel.moncarey@live.com>
2015-06-21 13:19:53 +0200

    Refactoring controller functionalities

--- src/Concerto/ControllerResolver.php
+++ src/Concerto/ControllerResolver.php
@@ -49,4 +49,38 @@ class ControllerResolver extends BaseControllerResolver
 
         return parent::doGetArguments($request, $controller, $parameters);
     }
+
+    /**
+     * Returns a callable for the given controller.
+     *
+     * @param string $controller A Controller string
+     *
+     * @return mixed A PHP callable
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function createController($controller)
+    {
+        if (false === strpos($controller, '::')) {
+            throw new \InvalidArgumentException(sprintf('Unable to find controller "%s".', $controller));
+        }
+
+        list($class, $method) = explode('::', $controller, 2);
+        
+        $class  = '\\controllers\\' . $class . 'Controller';
+        $method = $method . 'Action';
+
+        if (!class_exists($class)) {
+            throw new \InvalidArgumentException(sprintf('Class "%s" does not exist.', $class));
+        }
+
+        return array($this->instantiateController($class), $method);
+    }
+    
+    protected function instantiateController($class) {
+        
+        $class = $class . "Controller";
+        return new $class($this->app);
+        
+    }
 }
